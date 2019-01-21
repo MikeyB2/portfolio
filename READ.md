ee0a70175e54544791c94fdeb9b2da6ac4f0b096
Mike Bryant <mike.bryant.jr@icloud.com>
2019-01-21 15:52:56 -0700

    fixed issues and added some color corrections

--- READ.md
+++ READ.md
@@ -1,1 +1,7 @@
-# My Portfolio
+# Mike Bryant's Portfolio
+My Portfolio was written with HTML, CSS, JavaScript and jQuery.
+
+## Live Website
+Here is the link to my live
+[Portfolio Website](http://www.mbryantjr.com/).
+
--- main.js
+++ main.js
@@ -1,64 +1,47 @@
-!function (a) {
-    a(document).ready(function () {
-        var b = function () {
-            var b = !1;
-            return {
-                open: function () {
-                    var c = this;
-                    a(".wrapper").addClass("nav-open"),
-                        a(".header").addClass("nav-open"),
-                        b = !0,
-                        a(".wrapper").on("click", function () {
-                            c.close()
-                        })
-                },
-                close: function () {
-                    a(".wrapper").removeClass("nav-open"),
-                        a(".header").removeClass("nav-open"),
-                        b = !1,
-                        a(".wrapper").off("click")
-                },
-                mobileNavToggle: function (a) {
-                    a.stopPropagation(),
-                        b ? this.close() : this.open()
-                }
+
+$(document).ready(function () {
+    let b = function () {
+        let b = !1;
+        return {
+            open: function () {
+                let c = this;
+                $(".wrapper").addClass("nav-open"),
+                    $(".header").addClass("nav-open"),
+                    b = !0,
+                    $(".wrapper").on("click", function () {
+                        c.close()
+                    })
+            },
+            close: function () {
+                $(".wrapper").removeClass("nav-open"),
+                    $(".header").removeClass("nav-open"),
+                    b = !1,
+                    $(".wrapper").off("click")
+            },
+            mobileNavToggle: function (a) {
+                $.stopPropagation(),
+                    b ? this.close() : this.open()
             }
-        }();
-        a(".hamburger").stop().click(function (a) {
-            b.mobileNavToggle(a)
+        }
+    }();
+    $(".hamburger").stop().click(function (a) {
+        b.mobileNavToggle(a)
+    }),
+        $(".closeModal").click(function () {
+            b.close()
         }),
-            a(".closeModal").click(function () {
-                b.close()
-            }),
-            function () {
-                var b, c = 0;
-                a(window).scroll(function () {
-                    var d = a(this).scrollTop();
-                    d > c ? (b = d - 50,
-                        d > 100 && a(".header").finish().slideUp(300)) : b > d && a(".header").finish().slideDown(300),
-                        c = d
-                })
-            }(),
-            function () {
-                a(".navItem").click(function () {
-                    var b = a(this).data("section");
-                    a("html, body").animate({
-                        scrollTop: a("#" + b).offset().top
-                    })
-                }),
-                    a(".logo").click(function () {
-                        a("html, body").animate({
-                            scrollTop: 0
-                        })
-                    }),
-                    a(".mobile .navItem").click(function () {
-                        b.close()
-                    })
-            }(),
-            function () {
-                a(".workSection .hamburger, .workSection .close").click(function () {
-                    a(this).parent(".workSection").toggleClass("showing-more")
-                })
-            }()
-    })
-}(window.jQuery);
+        function () {
+            let b, c = 0;
+            $(window).scroll(function () {
+                let d = $(this).scrollTop();
+                d > c ? (b = d - 50,
+                    d > 100 && $(".header").finish().slideUp(300)) : b > d && $(".header").finish().slideDown(300),
+                    c = d
+            })
+        }(),
+        function () {
+            $(".workSection .hamburger, .workSection .close").click(function () {
+                $(this).parent(".workSection").toggleClass("showing-more")
+            })
+        }()
+})
